abc4de8eaf308e7b4b0a280fcf02a411f3fa7d5e
sigmajove <sigmajove@gmail.com>
2026-04-08 02:36:19 -0700

    Split the class denom_list out of kept.cc

---
+++ shared/denom_list.cc
@@ -0,0 +1,276 @@
+#include "denom_list.h"
+
+#include <algorithm>
+#include <cassert>
+
+#include "combin.h"
+
+denom_list::denom_list(int *left_table) : left(left_table) {
+  for (int j = 0; j <= num_suits; j++) avail[j] = 0;
+}
+
+int denom_list::no_pair(int n) {
+  assert(n >= 0);
+  // Everything is unrolled here; we know num_suits=4
+
+  const int sum4 = avail[4];
+  const int sum3 = avail[3] + sum4;
+  const int sum2 = avail[2] + sum3;
+  const int sum1 = avail[1] + sum2;
+
+  if (n > sum1) return 0;
+  // Make sure there are enough different denominations to select
+
+  // The idea here is that we will select
+  //
+  //    n = j1 + j2 + j3 + j4
+  //
+  // cards, where j1 is the number of singleton denominations chosen,
+  // j2 is the number of doubleton demominations chosen, and so on.
+  //
+  // For each quadruple (j1, j2, j3, j4), the number of ways to
+  // select the cards is
+  //
+  //  Count (j1, j2, j3, j4) =
+  //     choose (avail [1], j1) * 1**j1 *
+  //     choose (avail [2], j2) * 2**j2 *
+  //     choose (avail [3], j3) * 3**j3 *
+  //     choose (avail [4], j4) * 4**j4
+  //
+  // The first subterm counts the number of subsets of demoninations
+  // available, and the second counts the number of suit patterns
+  // available for that subset.
+  //
+  // We iterate over all possible values of (j1, j2, j3, j4) and
+  // sum up Count (j1, j2, j3, j4) for each.
+  //
+  // We find all possible values using four nested loops.
+  // Since j1 + j2 + j3 + j4 = n, j1 is bounded below by
+  // by n - avail[j2] - avail[j3] - avail[j4].  Likewise,
+  // j1 is bounded above by both avail[1] and n.  There are
+  // similar constraints at the next two nesting levels.
+  // In the innermost level, j4 is determined by the equation
+  // j4 = n - j3 - j2 - j1.
+
+  int result = 0;
+
+  const int min1 = std::max(n - sum2, 0);
+  const int max1 = std::min(avail[1], n);
+  for (int j1 = min1; j1 <= max1; j1++) {
+    int f1 = combin.choose(avail[1], j1);
+    n -= j1;
+
+    const int min2 = std::max(n - sum3, 0);
+    const int max2 = std::min(avail[2], n);
+    for (int j2 = min2; j2 <= max2; j2++) {
+      const int f2 = combin.choose(avail[2], j2) * (1 << j2) * f1;
+      // (1<<j2) == 2**j2
+
+      n -= j2;
+
+      const int min3 = std::max(n - sum4, 0);
+      const int max3 = std::min(avail[3], n);
+      for (int j3 = min3; j3 <= max3; j3++) {
+        int power = 1;
+        for (int jj = 0; jj < j3; jj++) power *= 3;
+        // power == 3**j3
+
+        const int f3 = combin.choose(avail[3], j3) * power * f2;
+
+        const int j4 = n - j3;
+        assert(0 <= j4 && j4 <= avail[4]);
+
+        const int f4 = combin.choose(avail[4], j4) * (1 << (2 * j4)) * f3;
+        // (1<<(2*j4)) == 4**j4
+
+        result += f4;
+      }
+
+      n += j2;
+    }
+
+    n += j1;
+  }
+
+  return result;
+}
+
+int denom_list::multi(int m, int n) {
+  assert(m >= 1);
+
+  if (m == 1) {
+    return no_pair(n);
+  }
+
+  int result = 0;
+
+  if (n >= m) {
+    for (int j = m; j <= 4; j++) {
+      if (avail[j] != 0) {
+        const int a = avail[j];
+        avail[j]--;
+        result += a * combin.choose(j, m) * no_pair(n - m);
+        avail[j]++;
+      }
+    }
+  }
+
+  return result;
+}
+
+int denom_list::one_pair(int n) {
+  if (n < 2) return 0;
+
+  int result = 0;
+
+  if (avail[2] != 0) {
+    int a = avail[2];
+    avail[2]--;
+    result += a * no_pair(n - 2);
+    avail[2]++;
+  }
+
+  if (avail[3] != 0) {
+    int a = avail[3];
+    avail[3]--;
+    result += a * 3 * no_pair(n - 2);
+    avail[3]++;
+  }
+
+  if (avail[4] != 0) {
+    int a = avail[4];
+    avail[4]--;
+    result += a * 6 * no_pair(n - 2);
+    avail[4]++;
+  }
+
+  return result;
+}
+
+int denom_list::triple(int n) {
+  if (n < 3) return 0;
+
+  int result = 0;
+
+  if (avail[3] != 0) {
+    int a = avail[3];
+    avail[3]--;
+    result += a * no_pair(n - 3);
+    avail[3]++;
+  }
+
+  if (avail[4] != 0) {
+    int a = avail[4];
+    avail[4]--;
+    result += a * 4 * no_pair(n - 3);
+    avail[4]++;
+  }
+
+  return result;
+}
+
+int denom_list::quad(int n) {
+  if (n < 4) return 0;
+
+  int result = 0;
+
+  if (avail[4] != 0) {
+    int a = avail[4];
+    avail[4]--;
+    result += a * no_pair(n - 4);
+    avail[4]++;
+  }
+
+  return result;
+}
+
+int denom_list::two_pair(int n) {
+  if (n < 4) return 0;
+
+  int result = 0;
+
+  if (avail[2] >= 2) {
+    int a = avail[2];
+    avail[2] -= 2;
+    result += combin.choose(a, 2) * no_pair(n - 4);
+    avail[2] += 2;
+  }
+
+  if (avail[3] >= 2) {
+    int a = avail[3];
+    avail[3] -= 2;
+    result += combin.choose(a, 2) * (3 * 3) * no_pair(n - 4);
+    avail[3] += 2;
+  }
+
+  if (avail[4] >= 2) {
+    int a = avail[4];
+    avail[4] -= 2;
+    result += combin.choose(a, 2) * (6 * 6) * no_pair(n - 4);
+    avail[4] += 2;
+    ;
+  }
+
+  if (avail[2] > 0 && avail[3] > 0) {
+    const int a = avail[2];
+    const int b = avail[3];
+
+    avail[2]--;
+    avail[3]--;
+    result += a * b * 3 * no_pair(n - 4);
+    avail[2]++;
+    avail[3]++;
+  }
+
+  if (avail[2] > 0 && avail[4] > 0) {
+    const int a = avail[2];
+    const int b = avail[4];
+
+    avail[2]--;
+    avail[4]--;
+    result += a * b * 6 * no_pair(n - 4);
+    avail[2]++;
+    avail[4]++;
+  }
+
+  if (avail[3] > 0 && avail[4] > 0) {
+    const int a = avail[3];
+    const int b = avail[4];
+
+    avail[3]--;
+    avail[4]--;
+    result += a * b * (3 * 6) * no_pair(n - 4);
+    avail[3]++;
+    avail[4]++;
+  }
+
+  return result;
+}
+
+int denom_list::full_house(int n) {
+  if (n < 5) return 0;
+
+  int result = 0;
+
+  if (avail[3] >= 2) {
+    result += avail[3] * (avail[3] - 1) * 3;
+  }
+
+  if (avail[4] >= 2) {
+    result += avail[4] * (avail[4] - 1) * (4 * 6);
+  }
+
+  if (avail[2] > 0 && avail[3] > 0) {
+    result += avail[2] * avail[3];
+  }
+
+  if (avail[2] > 0 && avail[4] > 0) {
+    result += avail[2] * avail[4] * 4;
+  }
+
+  if (avail[3] > 0 && avail[4] > 0) {
+    result += avail[3] * avail[4] * ((3 * 4) + 6);
+  }
+
+  return result;
+}
---
+++ shared/denom_list.h
@@ -0,0 +1,35 @@
+#include "vpoker.h"
+
+class denom_list {
+ public:
+  denom_list(int *left_table);
+
+  inline void add(int denom) { avail[left[denom]]++; }
+  inline void remove(int denom) { avail[left[denom]]--; }
+  // The client must not add the same denomination twice,
+  // and must only remove denominations that have been
+  // added.  There is no checking of these restrictions.
+
+  int no_pair(int n);
+  // returns the number of ways of selecting n cards that do
+  // not pair up (that is only one of each denomination).
+
+  int one_pair(int n);
+  int two_pair(int n);
+  int triple(int n);
+  int full_house(int n);
+  int quad(int n);
+
+  int multi(int m, int n);
+  // m = 2 => one_pair
+  // m = 3 => triple, etc.
+
+ private:
+  int *const left;
+
+  int avail[num_suits + 1];
+  // For each denomination added, we need to know the number of
+  // that kind of card is available.  This information is stored
+  // inverted-- for each number that might be available 0, 1, 2, 3, 4,
+  // we keep the number of denominations that have that availability.
+};
